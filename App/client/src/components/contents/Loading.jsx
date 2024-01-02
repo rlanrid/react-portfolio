1ed6d906cbdb30a1b7a2a896d9f111f2b666e192
rlanrid <rladnrider77@gmail.com>
2024-01-02 18:37:50 +0900

    배포

--- App/client/src/components/contents/Loading.jsx
+++ App/client/src/components/contents/Loading.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 
 const Loading = () => {
+
     return (
-        // <div className="ring">Loading
-        //     <span></span>
-        // </div>
-        <div class="e-loadholder">
-            <div class="m-loader">
-                <span class="e-text">Loading</span>
+        <>
+            <div className="e-loadholder">
+                <div className="m-loader">
+                    <span className="e-text">Loading</span>
+                </div>
             </div>
-        </div>
+            <div id="particleCanvas-Blue"></div>
+            <div id="particleCanvas-White"></div>
+        </>
     )
 }
 
